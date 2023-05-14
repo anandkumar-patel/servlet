9127597cd4a93a06d1fa7c24f8b39c02b09c69e6
anandkumar-patel <kumar.akpatel@gmail.com>
2023-05-14 18:14:43 +0530

    ajax call from jquery

--- WebContent/scripts/ajax/ajax-jquery.js
+++ WebContent/scripts/ajax/ajax-jquery.js
@@ -2,7 +2,7 @@ $(document).on("click", "#btn3", function() {
 	// execute ajax get request on url of "ExecuteServlet" and execute 
 	//the following function with ajax response text
 	$.ajax({
-        url: 'gettimeajax',
+        url: '/servlet/gettimeajax',
         method: 'GET',
         success: function (resultText) {
            $('#div3').html(resultText);
